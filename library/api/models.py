484a70f25bb252846603dddc4642ed1abe037e87
kamil <k.broniowski@osbix.com>
2025-05-11 21:14:07 +0200

    feat: add Book model with relevant tests

--- library/api/models.py
+++ library/api/models.py
@@ -11,3 +11,21 @@ class Reader(models.Model):
 
     def __str__(self):
         return self.serial_number
+
+
+class Book(models.Model):
+    serial_number = models.CharField(
+        max_length=6,
+        validators=[six_number_digits_validator],
+        unique=True,
+        primary_key=True,
+    )
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    borrower = models.ForeignKey(
+        Reader, on_delete=models.SET_NULL, null=True, blank=True
+    )
+    borrow_date = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.serial_number} {self.title} {self.author}"
--- tests/api/test_models.py
+++ tests/api/test_models.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
-from api.models import Reader
+from django.core.exceptions import ValidationError
+from django.utils import timezone
+from api.models import Reader, Book
 
 
 class ReaderModelIntegrationTest(TestCase):
@@ -25,3 +27,72 @@ class ReaderModelIntegrationTest(TestCase):
         filtered = Reader.objects.filter(serial_number__startswith="3")
         assert filtered.count() == 1
         assert filtered.first().serial_number == "333333"
+
+
+class BookModelTest(TestCase):
+    """focusing on model-specific aspects."""
+
+    def setUp(self):
+        """Set up test data for Book tests."""
+        self.reader = Reader.objects.create(serial_number="123456")
+        self.book = Book.objects.create(
+            serial_number="987654", title="Test Book", author="Test Author"
+        )
+
+    def test_book_primary_key(self):
+        """Test that serial_number functions as primary key."""
+        pk_field = Book._meta.pk
+        assert pk_field.name == "serial_number"
+
+        retrieved_book = Book.objects.get(pk="987654")
+        assert retrieved_book == self.book
+
+    def test_book_required_fields(self):
+        """Test validation for required fields."""
+        required_fields = ["serial_number", "title", "author"]
+
+        for field_name in required_fields:
+            with self.subTest(field=field_name):
+                book_data = {
+                    "serial_number": "555555",
+                    "title": "Test Title",
+                    "author": "Test Author",
+                }
+                book_data.pop(field_name)
+
+                book = Book(**book_data)
+                with self.assertRaises(ValidationError):
+                    book.full_clean()
+
+    def test_book_optional_fields(self):
+        """Test that optional fields can be null/blank."""
+        book = Book(
+            serial_number="444444",
+            title="Optional Fields Book",
+            author="Test Author",
+            borrower=None,
+            borrow_date=None,
+        )
+        book.full_clean()  # all fields are valid
+        book.save()
+
+        saved_book = Book.objects.get(serial_number="444444")
+        assert saved_book.borrower is None
+        assert saved_book.borrow_date is None
+
+    def test_book_foreign_key_relationship(self):
+        """Test the foreign key relationship between Book and Reader."""
+        self.book.borrower = self.reader
+        self.book.borrow_date = timezone.now()
+        self.book.save()
+
+        assert self.book.borrower.serial_number == "123456"
+
+        related_books = self.reader.book_set.all()
+        assert related_books.count() == 1
+        assert related_books.first() == self.book
+
+    def test_string_representation(self):
+        """Test the string representation of the Book model."""
+        expected_str = "987654 Test Book Test Author"
+        assert str(self.book) == expected_str

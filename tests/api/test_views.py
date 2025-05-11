fbbb4e0ea9a5a678111d0c181e33bed27fd45633
kamil <k.broniowski@osbix.com>
2025-05-11 20:33:03 +0200

    test: parametrizing method_not_allowed

--- tests/api/test_views.py
+++ tests/api/test_views.py
@@ -16,7 +16,7 @@ class TestReaderCreateAPIView:
         self.client = APIClient()
         self.url = reverse("reader-create")
 
-    def test_create_reader_returns_serial_number(self):
+    def test_create_reader_success(self):
         """
         POST /readers/ with with serial number returns 201 and serial number
         """
@@ -25,13 +25,6 @@ class TestReaderCreateAPIView:
         assert response.status_code == status.HTTP_201_CREATED
         assert response.data["serial_number"] == "123456"
         assert Reader.objects.filter(serial_number="123456").exists()
-
-    def test_read_reader_returns_method_unallowed(self):
-        """
-        GET /readers/ returns 405 Method Not Allowed
-        """
-        response = self.client.get(self.url)
-        assert response.status_code == status.HTTP_405_METHOD_NOT_ALLOWED
 
     def test_create_reader_with_invalid_serial_number(self):
         """
@@ -42,16 +35,18 @@ class TestReaderCreateAPIView:
         assert response.status_code == status.HTTP_400_BAD_REQUEST
         assert "serial_number" in response.data
 
-    def test_delete_reader_returns_method_unallowed(self):
+    @pytest.mark.parametrize(
+        "method, expected_status",
+        [
+            ("delete", status.HTTP_405_METHOD_NOT_ALLOWED),
+            ("put", status.HTTP_405_METHOD_NOT_ALLOWED),
+            ("get", status.HTTP_405_METHOD_NOT_ALLOWED),
+        ],
+    )
+    def test_reader_methods_not_allowed(self, method, expected_status):
         """
-        DELETE /readers/ returns 405 Method Not Allowed
+        Methods DELETE, PUT, GET on /readers/ return 405 Method Not Allowed
         """
-        response = self.client.delete(self.url)
-        assert response.status_code == status.HTTP_405_METHOD_NOT_ALLOWED
-
-    def test_update_reader_returns_method_unallowed(self):
-        """
-        PUT /readers/ returns 405 Method Not Allowed
-        """
-        response = self.client.put(self.url)
-        assert response.status_code == status.HTTP_405_METHOD_NOT_ALLOWED
+        client_method = getattr(self.client, method)
+        response = client_method(self.url)
+        assert response.status_code == expected_status

654b69f6ef3108e2905b5ef92878e8ef0e4e0d6f
kamil <k.broniowski@osbix.com>
2025-05-11 23:25:59 +0200

    chore: configure book endpoints with router

--- library/api/urls.py
+++ library/api/urls.py
@@ -1,6 +1,13 @@
-from django.urls import path
-from .views import ReaderCreateAPIView
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import ReaderCreateAPIView, BookViewSet
 
+# Create a router and register the ViewSet
+router = DefaultRouter()
+router.register(r"books", BookViewSet, basename="book")
+
+# The API URLs are determined automatically by the router
 urlpatterns = [
     path("readers/", ReaderCreateAPIView.as_view(), name="reader-create"),
+    path("", include(router.urls)),
 ]

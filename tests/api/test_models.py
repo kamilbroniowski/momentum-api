8548107cc2839b7458344c3002e37dd2fb9a9889
kamil <k.broniowski@osbix.com>
2025-05-11 20:50:18 +0200

    test: split bulc_create and filter_readers

--- tests/api/test_models.py
+++ tests/api/test_models.py
@@ -8,18 +8,20 @@ class ReaderModelIntegrationTest(TestCase):
     Using Django's TestCase class for transaction management.
     """
 
-    def test_bulk_create_and_filter(self):
-        """Test bulk creation and filtering of Reader objects."""
-        # Create multiple readers
+    def test_bulk_create_readers(self):
+        """Test bulk creation of Reader objects."""
         valid_serial_numbers = ["111111", "222222", "333333", "444444"]
-
         readers = [Reader(serial_number=sn) for sn in valid_serial_numbers]
         Reader.objects.bulk_create(readers)
 
-        # Test retrieving all readers
         assert Reader.objects.count() == 4
 
-        # Test filtering by serial number
+    def test_filter_readers_by_serial_number(self):
+        """Test filtering Reader objects by serial number."""
+        valid_serial_numbers = ["111111", "222222", "333333", "444444"]
+        readers = [Reader(serial_number=sn) for sn in valid_serial_numbers]
+        Reader.objects.bulk_create(readers)
+
         filtered = Reader.objects.filter(serial_number__startswith="3")
         assert filtered.count() == 1
         assert filtered.first().serial_number == "333333"
